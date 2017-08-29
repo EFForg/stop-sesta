0a85fd01833519919e4961299444c98f35a1394c
squiggy rubio <squiggy@eff.org>
2017-08-29 16:18:00 -0700

    Update compiled files

--- js/main.js
+++ js/main.js
@@ -7,6 +7,35 @@ $(function(){
         }
     });
 
+
+
+    $('a[href="#take-action"]').click(function () {
+      $('html, body').animate({
+        scrollTop: $("#take-action").offset().top - 15
+      }, 600);
+    });
+
+
+
+    $('a[href="#who-we-are"]').click(function () {
+      $('html, body').animate({
+        scrollTop: $("#who-we-are").offset().top - 15
+      }, 600);
+    });
+
+
+    $('a[href="#quotes"]').click(function () {
+      $('html, body').animate({
+        scrollTop: $("#quotes").offset().top - 20
+      }, 600);
+    });
+
+    $('a[href="#top"]').click(function () {
+      $('html, body').animate({
+        scrollTop: $("#top").offset().top - 15
+      }, 600);
+    });
+
     // Hide navbar on click.
     $('.nav a').on('click', function(){
       if ($('.navbar-toggle').css('display') != 'none') {
